ac1c187b77c73b7f60a5c7d613464145f8cba58b
miyamanae <enama0822@gmail.com>
2022-05-09 14:26:12 +0900

    test1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+     // asset()でURLを呼び出している個所でHTTPSでアクセスするようにする
+     if (config('app.env') === 'production') {
+        \URL::forceScheme('https');
+    }
     }
 }
